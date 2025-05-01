caac25fd76965c918a3cbdd959261f2b6a39a55f
Flokk <jack.oconnor767@gmail.com>
2025-05-01 14:30:43 +0800

    Enemy Highlighting
    Added functionality for enemies derived from the enemy class to highlight when hovered over

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,64 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+}
+	void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+
+	/**
+	 * Line Trace from cursor.
+	 * A. LastActor is null && ThisActor is null
+	 *		-Do Nothing
+	 * B. LastActor is null && ThisActor is valid
+	 *		-Highlight ThisActor
+	 * C. LastActor is valid && ThisActor is null
+	 *		-Unhighlight LastActor
+	 * D. Both actors are valid, but LastActor != ThisActor
+	 *		-Unhighlight LastActor, Highlight ThisActor
+	 * E. Both actors are valid, and are the same
+	 *		-Do nothing
+	 */
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			ThisActor->HighlightActor();
+		}
+	}
+	else //LastActor is Valid
+	{
+		if (ThisActor == nullptr)
+		{
+			LastActor->UnHighlightActor();
+		}
+		else //Both Actors are valid
+		{
+			if (LastActor != ThisActor)
+			{
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
